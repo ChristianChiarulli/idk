10a12a11c51fdaa139aa3ad2aac46aad8003edf1
Chrisatmachine <chrisatmachine@gmail.com>
2018-10-23 21:27:29 -0400

    added modifiable

---
+++ testdriven-app/services/users/project/config.py
@@ -0,0 +1,23 @@
+# services/users/project/config.py
+
+import os
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class TestingConfig(BaseConfig):
+    """Testing Configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
